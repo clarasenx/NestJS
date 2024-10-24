3a3e8da890a12faabf845c246e59ec6ff5b8134b
Clara Helena <clarahelenasena@gmail.com>
2024-10-24 09:24:19 -0400

    changes to learn about Dependency injection

--- src/recados/recados.module.ts
+++ src/recados/recados.module.ts
@@ -1,13 +1,29 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { RecadosService } from './recados.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecadosController } from './recados.controller';
 import { Recado } from './entities/recados.entity';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
+import { RecadosUtils } from './recados.utils';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Recado]), PessoasModule],
+  imports: [
+    TypeOrmModule.forFeature([Recado]), 
+    forwardRef(() => PessoasModule),
+  ],
   controllers: [RecadosController],
-  providers: [RecadosService],
+  providers: [
+    RecadosService,
+    {
+      provide: RecadosUtils,
+      useClass: RecadosUtils,
+    },
+  ],
+  exports: [
+    {
+      provide: RecadosUtils,
+      useClass: RecadosUtils,
+    },
+  ],
 })
 export class RecadosModule {}
